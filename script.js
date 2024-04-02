d5c3b8b6e74f48b8ff9d9bfb1910baf4ea2d856d
Noah Segal <n.h.segal@gmail.com>
2024-04-01 20:21:12 -0400

    Mirror and intersect detection

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ class Laser {
     this.on = false;
     this.dragging = false;
     this.turning = false;
-    this.beam = new Beam(posVec, directionVec)
+    this.beam = new Beam(createVector(posVec.x+30, posVec.y+30), directionVec)
  
   }
   move() {
@@ -102,17 +102,43 @@ class Beam {
     this.x = startPos.x;
     this.y = startPos.y;
     this.direction = headingVec;
+    this.segments = [];
+    this.segments.push(startPos);
   }
 
   display() {
-    stroke(250,0,0)
-    strokeWeight(10)
+    stroke(250,0,0);
+    strokeWeight(10);
+    for (let i = 0; i < this.segments.length-1; i++){
+      push();
+      line(this.segments[i].x+30*this.direction.x, this.y+30*this.direction.y, this.x+400*this.direction.x, this.y+400*this.direction.y)
+      pop();
+    }
     push();
-    line(this.x+30*this.direction.x, this.y+30*this.direction.y, this.x+400*this.direction.x, this.y+400*this.direction.y)
+    line(this.segments[this.segments.length-1].x+*this.direction.x, this.y+30*this.direction.y, this.x+400*this.direction.x, this.y+400*this.direction.y)
     pop();
+  }
+
+  checkAllMirrors() {
+
   }
   
 }
+
+class Mirror {
+  constructor(x1,y1,x2,y2){
+    this.x1 = x1;
+    this.y1 = y1;
+    this.x2 = x2;
+    this.y2 = y2;
+  }
+  display (){
+    stroke(100);
+    strokeWeight(10);
+    line(x1,y1,x2,y2)
+  }
+}
+
 
 function mousePressed() {
   laser.pickedUp();
@@ -120,4 +146,33 @@ function mousePressed() {
 function mouseReleased() {
   laser.dragging = false;
   laser.turning = false;
+}
+
+
+
+function intersectCheck(x1, y1, x2, y2, x3, y3, x4, y4) {
+  let p = createVector(x1,y1); 
+  let q = createVector(x3,y3); 
+  let r = createVector(x2-x1,y2-y1); 
+  let s = createVector(x4-x3,y4-y3); 
+  let denom = p5.Vector.cross(r, s).mag();
+  let diff = p5.Vector.sub(q, p);
+   
+  if (p5.Vector.cross(r, s).mag() == 0){
+
+    if (diff.cross(r).mag() == 0){
+      console.log('collinear')
+      return null
+    }
+     console.log('parallel')
+    return null
+  }
+  
+  let t = diff.cross(s).mag()/denom
+  let u = diff.cross(r).mag()/denom
+
+  if (t > 0 && t < 1 && u > 0 && u < 1){
+    return (p5.Vector.add(p, p5.Vector.mult(r, t)  ))
+  }
+  return null
 }
